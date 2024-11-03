79ac2e169899817fe2630be2d93c05f7af79c78f
Mohin uddin <Mohinuddin0119@gmail.com>
2024-11-03 11:50:16 +0600

    added update total

--- js/script.js
+++ js/script.js
@@ -9,13 +9,13 @@ for(const btn of allBtn){
         const price = event.target.parentNode.childNodes[3].childNodes[1].innerText
         // catagory
         const catagory = event.target.parentNode.childNodes[5].childNodes[1].innerText
-        console.log(catagory)
+        // console.log(catagory)
         const selectedElement = document.getElementById('selected-container')
 
         const div = document.createElement('div')
         div.classList.add('flex')
         div.classList.add('justify-center')
-        div.classList.add('gap-4')
+        div.classList.add('space-x-10')
         const p = document.createElement('p');
         p.innerText = name;
         const p2= document.createElement('p');
@@ -27,5 +27,30 @@ for(const btn of allBtn){
         div.appendChild(p2)
         div.appendChild(p3)
 
+        // update total and show total
+        updateTotalCost(price)
+        // update grand total and show grand
+        updateGrandTotal();
+
     })
+}
+
+function updateTotalCost(value){
+    const value1 = convertValue('total-cost');
+    console.log(value1)
+    const totalValue = value1 + parseInt(value);
+    setInnerText('total-cost',totalValue)
+}
+function updateGrandTotal(){
+    const totalCost = convertValue('total-cost');
+    setInnerText('grand-total',totalCost)
+}
+function setInnerText(id,value){
+    const element = document.getElementById(id);
+    element.innerText = value;
+}
+function convertValue(id){
+    const elementText = document.getElementById(id).innerText
+    const convertValue = parseInt(elementText);
+    return convertValue
 }
